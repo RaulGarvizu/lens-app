c65e6565879b2232a245e40fec9b5c6165a023ab
Raul Garvizu <raul.garvizu@jalasoft.com>
2022-07-22 18:40:03 -0400

    Added integration with Lens Smart Contract, connect and follow Profile

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Card from "@mui/material/Card"
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
-import { Container, Divider, Typography, ListItemText } from "@mui/material"
+import { Container, Typography, ListItemText } from "@mui/material"
 
 import { client, recommendProfiles } from '../api'
 
@@ -26,7 +26,7 @@ export default function Home() {
   async function fetchProfiles() {
     try {
       const response = await client.query(recommendProfiles).toPromise()
-      console.log({response})
+      console.log({ response })
       setProfiles(response.data.recommendedProfiles)
     } catch (error) {
       console.error({ error })
@@ -34,22 +34,22 @@ export default function Home() {
   }
 
   return (
-    <Container sx={{ padding: '20px' }}>
-      <Typography variant="h3" component="div" gutterBottom>
+    <Container sx={ { padding: 5 } }>
+      <Typography variant="h4" component="div" gutterBottom>
         Recommended Profiles
       </Typography>
-      <Masonry columns={4} spacing={3}>
+      <Masonry columns={ 4 } spacing={ 3 }>
         {
           profiles.map((profile, index) => (
-            <Link href={`/profile/${profile.id}`} key={index}>
-              <Card sx={{width: 250}} raised>
+            <Link href={ `/profile/${profile.id}` } key={ index }>
+              <Card sx={ { width: 250 } } raised>
                 <CardHeader
-                  avatar={<Avatar src={profile.picture?.original?.url} alt={profile.name} />}
-                  title={<ListItemText primary={profile.name} secondary={profile.handle} />}
+                  avatar={ <Avatar src={ profile.picture?.original?.url } alt={ profile.name } /> }
+                  title={ <ListItemText primary={ profile.name } secondary={ profile.handle } /> }
                 />
                 <CardContent>
                   <Typography variant="body2" color="text.secondary">
-                    {profile.bio}
+                    { profile.bio }
                   </Typography>
                 </CardContent>
               </Card>
--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -10,13 +10,19 @@ import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import { Avatar, Container, List, ListItem, ListItemText, Typography } from "@mui/material"
+import { Button, CardActions, Container, List, ListItem, ListItemText, Typography, Breadcrumbs, Link } from "@mui/material"
+
+import abi from '../../abi.json'
+import { ethers } from "ethers"
+
+const address = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d'
 
 export default function Profile() {
   const [profile, setProfile] = useState({})
   const router = useRouter()
   const { id } = router.query
   const [publications, setPublications] = useState([])
+  const [connected, setConnected] = useState(false)
 
   useEffect(() => {
     if (id) {
@@ -25,27 +31,50 @@ export default function Profile() {
     }
   }, [id])
 
+  async function connect() {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    })
+    console.log({ accounts })
+    setConnected(accounts.length > 0)
+  }
+
+  async function followUser() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+
+    const contract = new ethers.Contract(address, abi, signer)
+    try {
+      const tx = await contract.follow([id], [0x0])
+      await tx.wait()
+      console.log('followed user successfully')
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
+
   async function fetchPublications() {
     try {
-      const response = await client.query(getPublications, {id}).toPromise()
-      console.log({response})
+      const response = await client.query(getPublications, { id }).toPromise()
+      console.log({ response })
       setPublications(response.data?.publications?.items)
     }
     catch (err) {
-      console.error({err})
+      console.error({ err })
     }
   }
 
   async function fetchProfile() {
-    try{
+    try {
       const response = await client.query(getProfile, {
         id
       }).toPromise()
-      console.log({response})
+      console.log({ response })
       setProfile(response.data.profile)
     }
     catch (err) {
-      console.error({err})
+      console.error({ err })
     }
   }
 
@@ -59,43 +88,53 @@ export default function Profile() {
   }
 
   return (
-    <Stack direction='row'>
-      <List title="Publications" sx={{ maxWidth: publications.length > 0 ? 360 : 0, background: 'whitesmoke' }}>
-        {
-          publications.map((publication, index) => (
-            <ListItem key={index} alignItems="flex-start">
-              <ListItemText primary={publication.metadata?.name} secondary={publication.metadata?.description}/>
-            </ListItem>
-          ))
-        }
-      </List>
-      <Container style={floatingStyle} sx={{paddingTop: '20px'}}>
-        <Card raised sx={{ maxWidth: 400 }}>
-          <CardHeader title={<ListItemText primary={profile.name} secondary={profile.handle}/>}/>
-          <CardMedia
-            component='img'
-            height={profile.picture?.original?.url ? 500 : 0}
-            image={profile.picture?.original?.url}
-          />
-          <CardContent>
-            <Typography variant="body2" sx={{paddingBottom: '25px'}} >
-              {profile.bio}
-            </Typography>
-            <Stack direction='row' spacing={4}>
-              <Badge badgeContent={profile.stats?.totalFollowers} max={9999} color='primary'>
-                <Chip variant="outlined" color="primary" label="Followers"/>
-              </Badge>
-              <Badge badgeContent={profile.stats?.totalFollowing} max={9999} color='primary'>
-                <Chip variant="outlined" color="primary" label="Following"/>
-              </Badge>
-              <Badge badgeContent={profile.stats?.totalPosts} max={9999} color='primary'>
-                <Chip variant="outlined" color="primary" label="Posts"/>
-              </Badge>
-            </Stack>
-          </CardContent>
-        </Card>
-      </Container>
-    </Stack>
+    <>
+      <Breadcrumbs>
+        <Link underline="hover" color="inherit" href="/">Profiles</Link>
+        <Typography color='text.primary'>{ profile.handle }</Typography>
+      </Breadcrumbs>
+      <Button onClick={ connect } disabled={ connected } >Connect</Button>
+      <Stack direction='row'>
+        <Container style={ floatingStyle } sx={ { paddingTop: '20px' } }>
+          <Card raised sx={ { maxWidth: 400 } }>
+            <CardHeader title={ <ListItemText primary={ profile.name } secondary={ profile.handle } /> } />
+            <CardMedia
+              component='img'
+              height={ profile.picture?.original?.url ? 500 : 0 }
+              image={ profile.picture?.original?.url }
+            />
+            <CardContent>
+              <Typography variant="body2" sx={ { paddingBottom: '25px' } } >
+                { profile.bio }
+              </Typography>
+              <Stack direction='row' spacing={ 4 }>
+                <Badge badgeContent={ profile.stats?.totalFollowers } max={ 9999 } color='primary'>
+                  <Chip variant="outlined" color="primary" label="Followers" />
+                </Badge>
+                <Badge badgeContent={ profile.stats?.totalFollowing } max={ 9999 } color='primary'>
+                  <Chip variant="outlined" color="primary" label="Following" />
+                </Badge>
+                <Badge badgeContent={ profile.stats?.totalPosts } max={ 9999 } color='primary'>
+                  <Chip variant="outlined" color="primary" label="Posts" />
+                </Badge>
+              </Stack>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" onClick={ followUser }>Follow</Button>
+            </CardActions>
+          </Card>
+        </Container>
+        <List title="Publications" sx={ { maxWidth: publications.length > 0 ? 360 : 0, background: 'whitesmoke' } }>
+          {
+            publications.map((publication, index) => (
+              <ListItem key={ index } alignItems="flex-start">
+                <ListItemText primary={ publication.metadata?.name } secondary={ publication.metadata?.description } />
+              </ListItem>
+            ))
+          }
+        </List>
+      </Stack>
+    </>
   )
 }
 
